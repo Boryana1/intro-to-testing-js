ad9d4611b06d0a77bd8d520f890e34dc70b34634
Boryana1 <boryana.dimova1@gmail.com>
2022-03-23 16:55:43 -0500

    added Jane, Alex and Pat

--- tests.js
+++ tests.js
@@ -1,30 +1,36 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+        expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+        expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+        expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(helloWorld()).not.toBe(undefined);
     });
 });
 
-describe("sayHello", function(){
-    it('should be a defined function', function() {
+describe("sayHello", function () {
+    it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
     it('should return a string when called', function () {
         expect(typeof sayHello(name)).toBe('string');
     });
-    it('should return the string "Hello, Boryana" when executed', function () {
-       expect(sayHello("Boryana")).toBe('Hello, Boryana');
+    it('should return the string "Hello, Jane" when executed', function () {
+        expect(sayHello("Jane")).toBe('Hello, Jane')
     });
-    it("should never return 'undefined' when called", function() {
+    it('should return the string "Hello, Alex" when executed', function () {
+        expect(sayHello("Alex")).toBe('Hello, Alex')
+    });
+    it('should return the string "Hello, Pat" when executed', function () {
+        expect(sayHello("Pat")).toBe('Hello, Pat');
+    });
+    it("should never return 'undefined' when called", function () {
         expect(helloWorld(name)).not.toBe(undefined);
     });
 });
